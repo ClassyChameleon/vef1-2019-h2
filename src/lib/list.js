08ffd5c111fe425f537229888188dd0cb8de0072
Gísli Veigar <gisliveigar@hotmail.com>
2019-11-26 19:04:10 +0000

    index.js keyrist þegar síðan loadast. engin virkni

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,11 +1,14 @@
-import { empty } from './helpers';
+//import { empty } from './helpers';
 
-export default class List {
-  constructor() {
+/*export default class List {
+  /*constructor() {
     this.container = document.querySelector('.list');
-  }
+    console.log(this);
+    console.log(this.container);
+  }*//*
 
   load() {
-    empty(this.container);
+    const button = document.querySelectorAll('button');
+    console.log(button);
   }
-}
+}*/
